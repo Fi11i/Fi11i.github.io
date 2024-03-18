1ee59c404e25e135e75ef2d8da5aef61f6d2a0e5
lesoma <lsoma98@gmail.com>
2024-03-18 01:01:26 +0100

    Adding new project and restructuring prices section

--- main.js
+++ main.js
@@ -93,8 +93,8 @@ function rightArrowAction() {
     }
 }
 
-let slideId = ["project-1", "project-2", "project-3", "project-4", "project-5", "project-6", "project-7"];
-let slideIndex = [1,1,1,1,1,1,1];
+let slideId = ["project-1", "project-2", "project-3", "project-4", "project-5", "project-6", "project-7", "project-8", "project-9", "project-10", "project-11"];
+let slideIndex = [1,1,1,1,1,1,1,1,1,1,1];
 showSlides(1, 0);
 showSlides(1, 1);
 showSlides(1, 2);
@@ -102,6 +102,10 @@ showSlides(1, 3);
 showSlides(1, 4);
 showSlides(1, 5);
 showSlides(1, 6);
+showSlides(1, 7);
+showSlides(1, 8);
+showSlides(1, 9);
+showSlides(1, 10);
 
 function plusSlides(n, no) {
     showSlides(slideIndex[no] += n, no);
